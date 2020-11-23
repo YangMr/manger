6a5a20648edcd667a5d5c63050a09c019284b9a5
秦乐 <8085524+qin-music@user.noreply.gitee.com>
2020-11-23 10:56:39 +0800

    编辑登陆组件

---
+++ back-admin/src/views/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>我是登陆组件</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+  computed: {},
+  methods: {},
+};
+</script>
+<style lang="scss" scoped>
+</style>
